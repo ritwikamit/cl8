0473c24c57bde9288e2c69c3217108b464b555f8
Amit Chauhan <128042263+ritwikamit@users.noreply.github.com>
2026-05-31 01:07:36 +0530

    fix: remove non-existent tests directory from jest roots config

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/*.test.ts', '**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/bin/**',
+    '!src/types/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'clover'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  verbose: true,
+};
+
+module.exports = config;
