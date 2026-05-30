c8fa31f77342a17b30f8ea893f6223427342b395
ritwikamit <niharxdev@gmail.com>
2026-05-31 01:04:13 +0530

    Convert jest.config.ts to .js to avoid ts-node dependency

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
@@ -19,4 +18,4 @@ const config: Config = {
   verbose: true,
 };
 
-export default config;
+module.exports = config;
