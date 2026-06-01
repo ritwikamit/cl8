0150f46f0323dce12aee2c396441ba1ac6d45269
ritwikamit <niharxdev@gmail.com>
2026-06-01 15:14:14 +0530

    fix: disable ora stdin hijacking to prevent premature exit

--- src/ui/spinner-manager.ts
+++ src/ui/spinner-manager.ts
@@ -58,6 +58,7 @@ export class SpinnerManager {
       text: `${chalk.bold(config.icon)} ${config.text}...`,
       spinner: config.style,
       color: config.color,
+      discardStdin: false,
     }).start();
   }
 
